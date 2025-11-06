4722192635c1cdeaccbeed2e62b6cb88060113c7
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2025-11-06 16:13:02 +0530

    Import ButtonCounter component into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
+import ButtonCounter from "./components/ButtonCounter"; // import component
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Button Counter</h1>
+      <ButtonCounter />
     </div>
   )
 }
 
-export default App
+export default App;
